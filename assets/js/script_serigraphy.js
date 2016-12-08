a5154e5da5d88a7aca9a6c9f1edda0361c20f4e6
IgnacioGCL <ignaciogclavazza@hotmail.es>
2016-12-08 18:24:26 +0000

    Haciendo previsualizador

--- assets/js/script_serigraphy.js
+++ assets/js/script_serigraphy.js
@@ -7,3 +7,16 @@ document.addEventListener("DOMContentLoaded", function(){
         .delay(1700)
         .fadeOut();
 });
+
+$('#upload-file').change(function () {
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function () {
+        $('.div-image').css('background-image', 'url("' + reader.result + '")');
+        $('.div-image').css('background-color', 'rgba(0,0,0,0)');
+    }
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+    }
+});
